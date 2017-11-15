25e340313d1e8d8a67b3c1e755db612368efb39a
Václav Čadek <vaclavcadek@gmail.com>
2017-11-15 17:13:33 +0100

    Linear regression fix.

--- sklearn2pmml/models/regression.py
+++ sklearn2pmml/models/regression.py
@@ -32,20 +32,27 @@ class RegressionModel(Model):
         if self.pmml.model_name:
             regression_model.set('modelName', self.pmml.model_name)
         regression_model.append(self.mining_schema)
-        if self.function_name == 'classification':
-            regression_model.append(self.output)
         if self.local_transformations:
             regression_model.append(self.local_transformations)
-        for i, target_value in enumerate(reversed(self.pmml.target_values)):
+        if self.function_name == 'classification':
+            regression_model.append(self.output)
+            for i, target_value in enumerate(reversed(self.pmml.target_values)):
+                regression_table = ET.SubElement(regression_model, 'RegressionTable')
+                regression_table.set('targetCategory', target_value)
+                if i == len(self.pmml.target_values) - 1:
+                    intercept = '0'
+                else:
+                    intercept = np.atleast_1d(self.estimator.intercept_).astype(str)[0]
+                    for feature, coefficient in zip(self.pmml.feature_names, self.estimator.coef_.astype(str).ravel()):
+                        numeric_predictor = ET.SubElement(regression_table, 'NumericPredictor')
+                        numeric_predictor.set('name', '{}*'.format(feature) if self.local_transformations else feature)
+                        numeric_predictor.set('coefficient', coefficient)
+                regression_table.set('intercept', intercept)
+        else:
             regression_table = ET.SubElement(regression_model, 'RegressionTable')
-            regression_table.set('targetCategory', target_value)
-            if i == len(self.pmml.target_values) - 1:
-                intercept = '0'
-            else:
-                intercept = np.atleast_1d(self.estimator.intercept_).astype(str)[0]
-                for feature, coefficient in zip(self.pmml.feature_names, self.estimator.coef_.astype(str).ravel()):
-                    numeric_predictor = ET.SubElement(regression_table, 'NumericPredictor')
-                    numeric_predictor.set('name', '{}*'.format(feature) if self.local_transformations else feature)
-                    numeric_predictor.set('coefficient', coefficient)
-            regression_table.set('intercept', intercept)
+            for feature, coefficient in zip(self.pmml.feature_names, self.estimator.coef_.astype(str)):
+                numeric_predictor = ET.SubElement(regression_table, 'NumericPredictor')
+                numeric_predictor.set('name', '{}*'.format(feature) if self.local_transformations else feature)
+                numeric_predictor.set('coefficient', coefficient)
+            regression_table.set('intercept', self.estimator.intercept_.astype(str))
         return regression_model
